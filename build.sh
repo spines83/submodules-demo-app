94926ec8a123aaacdb6f057ed6f46a7b774d0762
Sean Pines <spines@vmware.com>
2024-01-12 18:02:14 -0700

    small updates

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 # Usage ./build.sh ENV
+# git clone git@github.com:spines83/submodules-demo-app.git --recurse-submodules
+git pull && git submodule update --remote
 
+ENV=$1 
+
+ytt -f base \
+    -f env/$ENV \
+    -f shared/env/$ENV
